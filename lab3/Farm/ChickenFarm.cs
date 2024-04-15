f59426282dfd4798475edaa09f023f113e908cb5
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-04-15 13:52:47 +0300

    improve logging

---
+++ lab3/Farm/ChickenFarm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class ChickenFarm : IFarm
+    {
+        public (string, string, int, double) RaiseAnimal(IAnimal animal) => ("Ферма курей", animal.Name, animal.Age, animal.FoodAmount);
+    }
+}
