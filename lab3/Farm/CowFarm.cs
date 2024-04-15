a626eb87af4841040ac70ffc915f4f73e87cda74
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-04-15 13:52:47 +0300

    Add new styles

---
+++ lab3/Farm/CowFarm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class CowFarm : IFarm
+    {
+        public (string, string, int, double) RaiseAnimal(IAnimal animal) => ("Ферма корів", animal.Name, animal.Age, animal.FoodAmount);
+    }
+}
