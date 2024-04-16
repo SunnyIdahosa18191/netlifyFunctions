8dbdda5291c28e3f14b6e90680dc47ed86b203f9
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-04-16 13:52:47 +0300

    Updated documentation

---
+++ lab3/Factory/SheepFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class SheepFactory : IFactory
+    {
+        public IAnimal createAnimal(string name, int age, double foodAmount) => new Sheep { Name = name, Age = age, FoodAmount = foodAmount };
+        public IFarm createFarm() => new SheepFarm();
+    }
+}
