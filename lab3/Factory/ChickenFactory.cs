a04c855d2272ba561e0ad63e20f6dbebc203085b
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-04-17 13:52:47 +0300

    Update dependencies to latest versions

---
+++ lab3/Factory/ChickenFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class ChickenFactory : IFactory
+    {
+        public IAnimal createAnimal(string name, int age, double foodAmount) => new Chicken { Name = name, Age = age, FoodAmount = foodAmount };
+        public IFarm createFarm() => new ChickenFarm();
+    }
+}
