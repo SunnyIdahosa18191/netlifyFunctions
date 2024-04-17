4bd23c48a06321cf5f3707813364eceacbe04190
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-04-17 13:52:47 +0300

    update config files

---
+++ lab3/Factory/CowFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class CowFactory : IFactory
+    {
+        public IAnimal createAnimal(string name, int age, double foodAmount) => new Cow { Name = name, Age = age, FoodAmount = foodAmount };
+        public IFarm createFarm() => new CowFarm();
+    }
+}
