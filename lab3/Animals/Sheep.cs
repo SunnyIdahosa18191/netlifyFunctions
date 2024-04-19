4af65b7aa85c1bfa4227527afd2e6f7ca8383bad
SunnyIdahosa18191 <SunnyIdahosa@protonmail.com>
2024-04-19 13:52:47 +0300

    update docker-compose.yml

---
+++ lab3/Animals/Sheep.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Sheep : IAnimal
+    {
+        public string Name { get; set; }
+        public double FoodAmount { get; set; }
+        public int Age { get; set; }
+    }
+}
